4712fd2ea2aeaf66842119b3c116eec0cb2a0e5e
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-05-24 11:10:27 +0530

    A new project cars added - redux review

---
+++ cars/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CarForm from './components/CarForm';
+import CarSearch from './components/CarSearch';
+import CarList from './components/CarList';
+import CarValue from './components/CarValue';
+
+const App = () => {
+  return (
+    <div className='container is-fluid'>
+      <CarForm />
+      <CarSearch />
+      <CarList />
+      <CarValue />
+    </div>
+  )
+}
+
+export default App
---
+++ cars/src/components/CarSearch.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store';
+
+const CarSearch = () => {
+  const dispatch = useDispatch();
+  const { searchTerm } = useSelector((state) => {
+    return state.cars;
+  }) 
+
+  const handleSearchTermChange = (event) => {
+    dispatch(changeSearchTerm(event.target.value))
+  }
+
+  return (
+    <div className='list-header'>
+      <h3 className='title is-3'>My Cars</h3>
+      <div className='search fiels is-horizontal'>
+        <label className='label'>Search</label>
+        <input className='input' value={searchTerm} onChange={handleSearchTermChange} />
+      </div>
+    </div>
+  )
+}
+
+export default CarSearch
